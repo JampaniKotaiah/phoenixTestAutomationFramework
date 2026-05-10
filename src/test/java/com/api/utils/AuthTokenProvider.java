eabcfc2f421c1a5de99339813a33720888a5bf98
jampani kotaiah <jampani.kotaiahqa@gmail.com>
2026-05-10 21:55:33 +0530

    somechangemade

--- src/test/java/com/api/utils/AuthTokenProvider.java
+++ src/test/java/com/api/utils/AuthTokenProvider.java
@@ -10,51 +10,34 @@ import com.api.pojo.UserDetailsPojo;
 import io.restassured.http.ContentType;
 
 public class AuthTokenProvider {
-	
+
 	private AuthTokenProvider() {
-		
+
 	}
 
 	public static String getToken(Role role) throws IOException {
-		
-		
+
 		UserDetailsPojo userDetailsPojo = null;
-		
-		
-		if(role == FD) {
-			
-			userDetailsPojo = new UserDetailsPojo("iamfd","password");
+
+		if (role == FD) {
+
+			userDetailsPojo = new UserDetailsPojo("iamfd", "password");
+		} else if (role == SUP) {
+
+			userDetailsPojo = new UserDetailsPojo("iamsup", "password");
+		} else if (role == ENG) {
+
+			userDetailsPojo = new UserDetailsPojo("iameng", "password");
+		} else if (role == QC) {
+
+			userDetailsPojo = new UserDetailsPojo("iamqc", "password");
 		}
-		else if(role == SUP) {
-			
-			userDetailsPojo = new UserDetailsPojo("iamsup","password");
-		}
-		else if(role == ENG) {
-			
-			userDetailsPojo = new UserDetailsPojo("iameng","password");
-		}
-		else if(role == QC) {
-			
-			userDetailsPojo = new UserDetailsPojo("iamqc","password");
-		}
-		
-		String token = given()
-			.baseUri(ConfigManager.getProperty("BASE_URI"))
-			.contentType(ContentType.JSON)
-			.body(userDetailsPojo)
-		.when()
-			.post("login")
-		.then()
-			.log().ifValidationFails()
-			.statusCode(200)
-			.body("message",equalTo("Success"))
-			.extract()
-			.body()
-			.jsonPath()
-			.getString("data.token");
-		
+
+		String token = given().baseUri(ConfigManager.getProperty("BASE_URI")).contentType(ContentType.JSON)
+				.body(userDetailsPojo).when().post("login").then().log().ifValidationFails().statusCode(200)
+				.body("message", equalTo("Success")).extract().body().jsonPath().getString("data.token");
+
 		return token;
-		
 
 	}
 
